c3780e782d4a76e543b59d662a6b5cf9958ee627
Ignacio <icg837@gmail.com>
2018-08-21 13:08:21 +0200

    Update update-kernel.sh

--- Tutorials/update-kernel.sh
+++ Tutorials/update-kernel.sh
@@ -6,13 +6,12 @@ eselect kernel list
 # [1] linux-x.x.x1-gentoo *
 # [2] linux-x.x.x2-gentoo
 
-rm linux ## Estando en el directorio /usr/src
-
 ## Paso 1º: de manera automática
 
 eselect kernel set 2
 
 ## Paso 2º: de manera manual
+rm linux ## Estando en el directorio /usr/src
 ln -s linux-x.x.x2-gentoo/ linux
 ls ## Para comprobar que vuelve a haber un enlace simbólico tras borrarlo anteriormente
 
@@ -24,6 +23,15 @@ ls ## Para comprobar que vuelve a haber un enlace simbólico tras borrarlo anter
 
 cd /usr/src/linux
 cp ../linux-x.x.x1-gentoo/.config .oldconfig
+
+## Paso 1º: genkernel
+
+module-rebuild populate
+genkernel --olconfig --menuconfig all
+module-rebuild rebuild
+
+## Paso 2º: manual
+
 make olddefconfig
 make menuconfig ## Opcional, sólo en caso de activar opciones no activadas en el núcleo anterior
 make -j5 ## Al igual que en /etc/portage/make.conf, se debe poner -j seguido del número de núcleos del ordenador más 1
@@ -34,6 +42,8 @@ make install
 
 ## En este punto, copiar y/o actualizar las líneas de tipo modules_x_x_x_gentoo="vboxdrv vboxnetflt vboxnetadp vboxvideo" a
 ## /etc/conf.d/modules
+
+##
 
 grub2-mkconfig -o /boot/grub/grub.cfg
 reboot
