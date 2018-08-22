8eb92d7de17fb84f47e512587059aa8159fe7364
Ignacio <icg837@gmail.com>
2018-08-22 09:07:41 +0000

    Update emerge.sh

--- Tutorials/emerge.sh
+++ Tutorials/emerge.sh
@@ -1,1 +1,6 @@
-
+## The following USE changes are necessary to proceed:
+## # required by foo-foo/foo-x.x.x::gentoo
+## # required by foo-foo/foo (argument)
+## >=foo2-foo2/foo2.x.x.x USEflag
+nano -w /etc/portage/package.use/package.use
+# Copiar en ese archivo la línea donde se especifica el USE flag a añadir
