cf5a4fce337bde1bb7f20efefde701e7b729be09
Ignacio <icg837@gmail.com>
2018-08-22 10:31:43 +0000

    Update emerge.sh

--- Tutorials/emerge.sh
+++ Tutorials/emerge.sh
@@ -4,3 +4,11 @@
 ## >=foo2-foo2/foo2.x.x.x USEflag
 nano -w /etc/portage/package.use/package.use
 # Copiar en ese archivo la línea donde se especifica el USE flag a añadir
+
+## The following keyword changes are necessary to proceed:
+## # required by foo-foo/foo (argument)
+## =foo-foo/foo-x.x.x ~amd64
+emerge --autounmask-write foo-foo/foo
+dispatch-conf
+# Pulsar la tecla "u" para use-new y salir con la tecla "q"
+emerge -av foo-foo/foo
