55b4021c195a0597eec0d185ac30351eeeb7de1b
Ignacio <icg837@gmail.com>
2018-08-22 11:32:44 +0000

    Update install.sh

--- Tutorials/install.sh
+++ Tutorials/install.sh
@@ -175,7 +175,7 @@ time emerge -qav sys-apps/mlocate
 time emerge -qav net-wireless/iw
 echo 'GRUB_PLATFORMS="efi-64"' >> /etc/portage/make.conf
 time emerge -qav sys-boot/grub:2
-grub-install --target=x86_64-efi --efi-directory=/boot/efi
+grub-install --target=x86_64-efi --efi-directory=/boot
 grub-mkconfig -o /boot/grub/grub.cfg
 
 ## Opcional
