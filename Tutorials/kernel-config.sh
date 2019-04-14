7fb10265c5566fca8d9499faac965b868dd15bd6
Ignacio <icg837@gmail.com>
2019-04-14 13:57:19 +0200

    Update kernel-config.sh

--- Tutorials/kernel-config.sh
+++ Tutorials/kernel-config.sh
@@ -1,5 +1,5 @@
 ## Este archivo solo señalará aquellas configuraciones que se modifiquen para el equipo en cuestión.
-## Antes de nada, ejecutar los comandos lspci -v, y lsusb -V, para conocer qué módulos incluir o no.
+## Antes de nada, ejecutar los comandos lspci -v, y lsusb -v, para conocer qué módulos incluir o no.
 
 make menuconfig
 
