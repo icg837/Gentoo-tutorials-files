76853da4b738220dca259946ebe8ce4a3054b87a
Ignacio <icg837@gmail.com>
2019-04-14 18:50:22 +0200

    Update install.sh

--- Tutorials/install.sh
+++ Tutorials/install.sh
@@ -205,7 +205,7 @@ nano -w /etc/inittab
 time emerge -av sysklogd cronie mlocate ccache flaggie gentoolkit sudo
 rc-update add sysklogd default
 rc-update add cronie default
-emerge -av dosfstools dhcpcd wpa_supplicant wireless-tools
+emerge -av dosfstools dhcpcd wpa_supplicant wireless-tools xf86-input-evdev
 touch /etc/wpa_supplicant/wpa_supplicant.conf
 time emerge -av grub
 mount -o remount,rw /sys/firmware/efi/efivars
